c6c3f84f0d94dc3a41373c76c7217be4acd25b7a
santiago <scotto@technisys.com>
2022-09-25 10:58:37 -0300

    fixes

--- index.js
+++ index.js
@@ -19,33 +19,93 @@ function uuidv4() {
 
 (function (window) {
 
+    
     // declare
-    var gs = {};
+    var gs = {
+        props : {}
+    };
+
+    gs.log = function(str){
+        if (gs && gs.debug){
+            console.log(str);
+        }
+    }
 
     const URL = 'https://go-search-api.dev.goshops.com/reco/';
-    
+    const GS_SESSION = 'gs-session';
+    const GS_PROPS = 'gs-props';
+
     var headers = new Headers();
     headers.append("Content-Type", "application/json");
 
-    gs.init = function (code) {
+    gs.init = function (code, opts) {
         // ...
+        if (!opts) opts = {};
+
+        gs.debug = opts.debug;
+
+        gs.log('GoShops Init <', code,'>');
+
+        gs.props = gs.load(); //load stored info
+
+        if (!sessionStorage.getItem(GS_SESSION)){
+            sessionStorage.setItem(GS_SESSION, uuidv4())
+            gs.log('Init anon session', sessionStorage.getItem(GS_SESSION))
+
+            gs.user = sessionStorage.getItem(GS_SESSION);
+        }
         
-        console.log('GoShops Init <', code,'>');
-
-        gs.id = uuidv4();
-        console.log(gs.id);
-
     };
 
-    gs.session = function(userId){
-        gs.user = userId;
+    gs.save = function(){
+        sessionStorage.setItem(GS_PROPS, JSON.stringify(gs.props))
+    }
+    gs.load = function(){
+        try{
+            return JSON.parse(sessionStorage.getItem(GS_PROPS))
+        }catch(e){
+            return {};
+        }
+    }
+
+
+    gs.reset = function(){
+        sessionStorage.removeItem(GS_SESSION);
+        sessionStorage.removeItem(GS_PROPS);
+    }
+
+    gs.feeback = function(event, obj){
+        var raw = JSON.stringify({
+            "event": event,
+            "id": uuidv4(),
+            "fields": obj.fields || [],
+            "ranking" : gs.ranking,
+            "item": obj.item,
+            "user": gs.user,
+            "session": gs.user,
+            "timestamp": new Date().getTime()
+        }); 
+
+        fetch(URL + "feedback", {
+            method: 'POST',
+            headers: headers,
+            body: raw,
+            redirect: 'follow'
+        })
+    }
+    gs.setUser = function(userId){
+        gs.props.user = userId;
+        gs.save();
     }
 
     gs.rank = async function(items){
 
+        let id = uuidv4();
+        gs.ranking = id;
+
         var raw = JSON.stringify({
             "event": "ranking",
-            "id": gs.id,
+            "id": id,
             "items": items,
             "user": gs.user,
             "session": gs.user,
@@ -66,7 +126,7 @@ function uuidv4() {
             const rawFeedback = JSON.stringify({
                 "event": "ranking",
                 "fields": [],
-                "id": gs.id,
+                "id": id,
                 "items":items,
                 "user": gs.user,
                 "session": gs.user,
@@ -89,8 +149,6 @@ function uuidv4() {
           
     }
 
-    // define your namespace myApp
     window.gs = gs;
-    // gs.init();
 
 })(window, undefined);
